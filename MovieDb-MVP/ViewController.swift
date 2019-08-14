e666b2d6daf23ceb15b34f3c34a12af68d8d1795
guilhermeRangel <gui.vg16@hotmail.com>
2019-08-14 15:10:16 -0300

    init

---
+++ MovieDb-MVP/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MovieDb-MVP
+//
+//  Created by Guilherme Rangel on 14/08/19.
+//  Copyright © 2019 Guilherme Rangel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
