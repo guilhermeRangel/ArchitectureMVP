ad9391da9b03585085819090ed9ca542e15fca7a
guilhermeRangel <gui.vg16@hotmail.com>
2019-08-15 16:54:37 -0300

    create class model

---
+++ MovieDb-MVP/Model/InterfaceModel.swift
@@ -0,0 +1,13 @@
+//
+//  InterfaceModel.swift
+//  MovieDb-MVP
+//
+//  Created by Guilherme Rangel on 15/08/19.
+//  Copyright © 2019 Guilherme Rangel. All rights reserved.
+//
+
+import Foundation
+
+protocol InterfaceModel {
+    
+}
